e955f8f7b33abb6345cc5d4d6d1387868ab974f8
Sonja Allani <sallani@pc01.pool.tp1.uni-duesseldorf.de>
2016-02-02 11:55:43 +0100

    Lichtwellenleiter

--- nlsi.cxx
+++ nlsi.cxx
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <complex>
 #include <sstream>
+#include <cmath>
 //-----------------------------------
 using namespace std;
 //-----------------------------------
@@ -12,6 +13,9 @@ void init( cmplx* const psi0, const double eta, const double sigma, const double
 
 void writeToFile(const cmplx* const v, const string s, const double dx,
                  const int Nx, const double xmin);
+void Lstep(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx, const int N);
+void Nstep(cmplx* psi0, cmplx* psiS, const double dt, const int N);
 //-----------------------------------
 int main(){
 
@@ -29,7 +33,8 @@ int main(){
 	stringstream strm;
 
 	cmplx* psi0 = new cmplx[Nx];
-
+	cmplx* psiS = new cmplx[Nx]; // Psi tilde als Zwischenergebnis
+	cmplx* h;
 	init(psi0, eta, dx, dt,Nx);
 
 	writeToFile(psi0,"psi_0", dx,Nx,xmin);
@@ -38,12 +43,21 @@ int main(){
 	for (int i = 1; i <= Na; i++) {
 
 		for (int j = 1; j <= Nk-1; j++) {
+		  
+		  Lstep(psiS, psi0, dt, dx, Nx); // 1. Schritt der linearen Gleichung (vgl Mitschrift); Jetzt ist das alte Psi in psi0 und das neue in psiS gespeichert
+		  
+		  Nstep(psi0, psiS, dt,Nx); // 2. Schritt ""0; wir errechnen jetzt das neue psi und nutzen dabei psiS; dazu überschreiben wir direkt psi0, was dann im nächsten Schritt direkt wieder genutzt wird
+// 		  h = psi0;
+// 		  psi0 = psiS;
+// 		  psiS = h; 
+		  
 		}
 		strm.str("");
 		strm << "psi_" << i;
 		writeToFile(psi0,strm.str(), dx,Nx,xmin);
 	}
-
+	delete[] psi0;
+	delete[] psiS;
 	return 0;
 }
 //-----------------------------------
@@ -66,6 +80,39 @@ void init( cmplx* const psi0, const double eta,  const double dx, const double d
 	const double f = sqrt(2) * eta;
 	for(int i=0;i<Nx; i++){
 		double x = i*dx - x0;
-		psi0[i] = 2*f/cosh(eta * x);
+		psi0[i] =2* f/cosh(eta * x); // Anfangshöhe ändert Verlauf der Fkt (Versuche mit * 2 weg)
 	}
 }
+void Lstep(cmplx* const f1, cmplx* const f0,
+          const double dt, const double dx, const int N) //aus lab 11 geklaut
+{
+
+  cmplx* d=new cmplx[N];
+  cmplx* u=new cmplx[N];
+  cmplx* l=new cmplx[N];
+
+  for(int i=0;i<N;i++) d[i] = cmplx(1.0,-2.0*dt/(dx*dx));
+  for(int i=0;i<N;i++) u[i] = cmplx(0.0,dt/(dx*dx));
+  for(int i=0;i<N;i++) l[i] = cmplx(0.0,dt/(dx*dx));
+  
+  for(int i=1;i<N;i++){
+  
+    d[i]  -= u[i-1]*l[i]/d[i-1];
+    f0[i] -= f0[i-1]*l[i]/d[i-1];
+  }
+  
+  f1[N-1] = f0[N-1]/d[N-1];  //f0 und d sind schon die geschlaengelten
+  
+  for(int i=N-2;i>=0;i--){
+  
+    f1[i] = (f0[i] - u[i]*f1[i+1]) / d[i];    
+  }
+}
+  void Nstep(cmplx* psi0, cmplx* psiS, const double dt, const int N){
+  
+    for (int i=0; i<N;i++){
+    cmplx v = cmplx(0,-norm(psiS[i])*dt); 
+    psi0[i] = psiS[i] * exp(v);
+    }
+  }
+  
